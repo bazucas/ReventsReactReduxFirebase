bd6f4568454f8dd0b00f28df5cb7fd16cfcc3bb2
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-11 12:33:26 +0100

    Test html

--- revents/src/index.js
+++ revents/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-react'
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
