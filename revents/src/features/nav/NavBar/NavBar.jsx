48da7cc6393b923c6ccafc63de72db1c31fc9155
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-13 14:38:26 +0100

    Login and Register Modals

--- revents/src/features/nav/NavBar/NavBar.jsx
+++ revents/src/features/nav/NavBar/NavBar.jsx
@@ -1,29 +1,39 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 import { Menu, Container, Button } from "semantic-ui-react";
 import { NavLink, Link, withRouter } from "react-router-dom";
 import SignedOutMenu from "../Menus/SignedOutMenu";
 import SignedInMenu from "../Menus/SignedInMenu";
+import { openModal } from '../../modals/modalActions'
+import { logout } from '../../auth/authActions'
+
+const actions = {
+  openModal,
+  logout
+}
+
+const mapState = (state) => ({
+  auth: state.auth
+})
 
 class NavBar extends Component {
-  state = {
-    authenticated: false
-  };
 
   handleSignIn = () => {
-    this.setState({
-      authenticated: true
-    });
+    this.props.openModal('LoginModal')
+  };
+
+  handleRegister = () => {
+    this.props.openModal('RegisterModal')
   };
 
   handleSignOut = () => {
-    this.setState({
-      authenticated: false
-    });
+    this.props.logout();
     this.props.history.push("/");
   };
 
   render() {
-    const { authenticated } = this.state;
+    const { auth } = this.props;
+    const authenticated = auth.authenticated;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -49,13 +59,13 @@ class NavBar extends Component {
             />
           </Menu.Item>}
           {authenticated ? (
-            <SignedInMenu signOut={this.handleSignOut} />
+            <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut} />
           ) : (
-            <SignedOutMenu signIn={this.handleSignIn} />
+            <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister} />
           )}
         </Container>
       </Menu>
     );
   }
 }
-export default withRouter(NavBar); //because NavBar in App.jsx doesn't have any routing
+export default withRouter(connect(mapState, actions)(NavBar)); //because NavBar in App.jsx doesn't have any routing
