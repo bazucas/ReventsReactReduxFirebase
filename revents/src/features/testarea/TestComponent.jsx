4bf1d1d73e9c66fb9e076a6e2533e7a802b5ca88
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-13 16:13:55 +0100

    Redux Thunk added with issue

--- revents/src/features/testarea/TestComponent.jsx
+++ revents/src/features/testarea/TestComponent.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
-import { incrementCounter, decrementCounter } from "./testActions";
+import { incrementAsync, decrementAsync } from "./testActions";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
@@ -9,12 +9,13 @@ import PlacesAutocomplete, {
 import { openModal } from '../modals/modalActions'
 
 const mapState = state => ({
-  data: state.test.data
+  data: state.test.data,
+  loading: state.test.loading
 });
 
 const actions = {
-  incrementCounter,
-  decrementCounter,
+  incrementAsync,
+  decrementAsync,
   openModal
 };
 
@@ -52,13 +53,13 @@ class TestCompoment extends Component {
       value: this.state.address,
       onChange: this.onChange
     };
-    const { incrementCounter, decrementCounter, data, openModal } = this.props;
+    const { incrementAsync, decrementAsync, data, openModal, loading } = this.props;
     return (
       <div>
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
-        <Button onClick={incrementCounter} color="green" content="Increment" />
-        <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button loading={loading} onClick={incrementAsync} color="green" content="Increment" />
+        <Button loading={loading} onClick={decrementAsync} color="red" content="Decrement" />
         <Button onClick={() => openModal('TestModal', {data: 43})} color="teal" content="Open Modal" />
         <br />
         <br />
