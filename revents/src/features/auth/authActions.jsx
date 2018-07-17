a97e2f912d9ce22ec5d6e9fa461dcdc9cba7a175
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-17 13:14:20 +0100

    Facebook login (need to get api from fb dev page and set it in firebase)

--- revents/src/features/auth/authActions.jsx
+++ revents/src/features/auth/authActions.jsx
@@ -44,3 +44,41 @@ export const registerUser = user => async (dispatch, getState, { getFirebase, ge
     });
   }
 };
+
+export const socialLogin = (selectedProvider) => 
+  async (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      dispatch(closeModal());
+      let user = await firebase.login({
+        provider: selectedProvider,
+        type: 'popup'
+      })
+      if (user.additionalUserInfo.isNewUser) {
+          await firestore.set(`users/${user.user.uid}`, {
+            displayName: user.profile.displayName,
+            photoURL: user.profile.avatarUrl,
+            createdAt: firestore.FieldValue.serverTimestamp()
+          })
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  export const updatePassword = (creds) => 
+    async (dispatch, getState, {getFirebase}) => {
+      const firebase = getFirebase();
+      const user = firebase.auth().currentUser;
+      try {
+        await user.updatePassword(creds.newPassword1);
+        await dispatch(reset('account'));
+        toastr.success('Success!', 'Your password has been updated');
+      } catch (error) {
+        throw new SubmissionError({
+          _error: error.message
+        })
+      }
+
+    }
