d239c426a067c260a70c9a125e1a11d93c352971
Luís Ricardo Silva Inácio <luis.inacio@aitec.pt>
2018-07-20 13:30:18 +0100

    Securing application

--- revents/src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ revents/src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Segment, Image, Item, Header, Button } from 'semantic-ui-react';
-import format from 'date-fns/format';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
+import format from "date-fns/format";
+import { Link } from "react-router-dom";
 
 const eventImageStyle = {
-  filter: 'brightness(30%)'
+  filter: "brightness(30%)"
 };
 
 const eventImageTextStyle = {
-  position: 'absolute',
-  bottom: '5%',
-  left: '5%',
-  width: '100%',
-  height: 'auto',
-  color: 'white'
+  position: "absolute",
+  bottom: "5%",
+  left: "5%",
+  width: "100%",
+  height: "auto",
+  color: "white"
 };
 
 const EventDetailedHeader = ({
@@ -22,19 +22,31 @@ const EventDetailedHeader = ({
   isHost,
   isGoing,
   goingToEvent,
-  cancelGoingToEvent
+  cancelGoingToEvent,
+  authenticated,
+  openModal
 }) => {
   return (
     <Segment.Group>
-      <Segment basic attached="top" style={{ padding: '0' }}>
-        <Image src={`/assets/categoryImages/${event.category}.jpg`} fluid style={eventImageStyle} />
+      <Segment basic attached="top" style={{ padding: "0" }}>
+        <Image
+          src={`/assets/categoryImages/${event.category}.jpg`}
+          fluid
+          style={eventImageStyle}
+        />
 
         <Segment basic style={eventImageTextStyle}>
           <Item.Group>
             <Item>
               <Item.Content>
-                <Header size="huge" content={event.title} style={{ color: 'white' }} />
-                <p>{format(event.date && event.date.toDate(), 'dddd Do MMMM')}</p>
+                <Header
+                  size="huge"
+                  content={event.title}
+                  style={{ color: "white" }}
+                />
+                <p>
+                  {format(event.date && event.date.toDate(), "dddd Do MMMM")}
+                </p>
                 <p>
                   Hosted by <strong>{event.hostedBy}</strong>
                 </p>
@@ -47,10 +59,27 @@ const EventDetailedHeader = ({
       <Segment attached="bottom">
         {!isHost && (
           <div>
-            {isGoing ? (
-              <Button onClick={() => cancelGoingToEvent(event)}>Cancel My Place</Button>
-            ) : (
-              <Button loading={loading} onClick={() => goingToEvent(event)} color="teal">
+            {isGoing && (
+              <Button onClick={() => cancelGoingToEvent(event)}>
+                Cancel My Place
+              </Button>
+            )}
+            {!isGoing &&
+              authenticated && (
+                <Button
+                  loading={loading}
+                  onClick={() => goingToEvent(event)}
+                  color="teal"
+                >
+                  JOIN THIS EVENT
+                </Button>
+              )}
+            {!authenticated && (
+              <Button
+                loading={loading}
+                onClick={() => openModal("UnauthModal")}
+                color="teal"
+              >
                 JOIN THIS EVENT
               </Button>
             )}
--- revents/src/features/event/EventDetailed/EventDetailedPage.jsx
+++ revents/src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { withFirestore, firebaseConnect, isEmpty } from 'react-redux-firebase';
-import { compose } from 'redux';
-import EventDetailedHeader from './EventDetailedHeader';
-import EventDetailedInfo from './EventDetailedInfo';
-import EventDetailedChat from './EventDetailedChat';
-import EventDetailedSidebar from './EventDetailedSidebar';
-import { objectToArray, createDataTree } from '../../../app/common/util/helpers';
-import { goingToEvent, cancelGoingToEvent } from '../../user/userActions';
-import { addEventComment } from '../eventActions';
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { withFirestore, firebaseConnect, isEmpty } from "react-redux-firebase";
+import { compose } from "redux";
+import EventDetailedHeader from "./EventDetailedHeader";
+import EventDetailedInfo from "./EventDetailedInfo";
+import EventDetailedChat from "./EventDetailedChat";
+import EventDetailedSidebar from "./EventDetailedSidebar";
+import {
+  objectToArray,
+  createDataTree
+} from "../../../app/common/util/helpers";
+import { goingToEvent, cancelGoingToEvent } from "../../user/userActions";
+import { addEventComment } from "../eventActions";
+import { openModal } from "../../modals/modalActions";
 
 const mapState = (state, ownProps) => {
   let event = {};
@@ -20,6 +24,7 @@ const mapState = (state, ownProps) => {
 
   return {
     event,
+    loading: state.async.loading,
     auth: state.firebase.auth,
     eventChat:
       !isEmpty(state.firebase.data.event_chat) &&
@@ -30,7 +35,8 @@ const mapState = (state, ownProps) => {
 const actions = {
   goingToEvent,
   cancelGoingToEvent,
-  addEventComment
+  addEventComment,
+  openModal
 };
 
 class EventDetailedPage extends Component {
@@ -45,23 +51,43 @@ class EventDetailedPage extends Component {
   }
 
   render() {
-    const { event, auth, goingToEvent, cancelGoingToEvent, addEventComment, eventChat } = this.props;
-    const attendees = event && event.attendees && objectToArray(event.attendees);
+    const {
+      openModal,
+      event,
+      auth,
+      goingToEvent,
+      cancelGoingToEvent,
+      addEventComment,
+      eventChat,
+      loading
+    } = this.props;
+    const attendees =
+      event && event.attendees && objectToArray(event.attendees);
     const isHost = event.hostUid === auth.uid;
     const isGoing = attendees && attendees.some(a => a.id === auth.uid);
-    const chatTree = !isEmpty(eventChat) && createDataTree(eventChat)
+    const chatTree = !isEmpty(eventChat) && createDataTree(eventChat);
+    const authenticated = auth.isLoaded && !auth.isEmpty;
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventDetailedHeader
             event={event}
+            loading={loading}
             isHost={isHost}
             isGoing={isGoing}
             goingToEvent={goingToEvent}
             cancelGoingToEvent={cancelGoingToEvent}
+            authenticated={authenticated}
+            openModal={openModal}
           />
           <EventDetailedInfo event={event} />
-          <EventDetailedChat addEventComment={addEventComment} eventId={event.id} eventChat={chatTree} />
+          {authenticated && (
+            <EventDetailedChat
+              addEventComment={addEventComment}
+              eventId={event.id}
+              eventChat={chatTree}
+            />
+          )}
         </Grid.Column>
         <Grid.Column width={6}>
           <EventDetailedSidebar attendees={attendees} />
@@ -73,6 +99,9 @@ class EventDetailedPage extends Component {
 
 export default compose(
   withFirestore,
-  connect(mapState, actions),
+  connect(
+    mapState,
+    actions
+  ),
   firebaseConnect(props => [`event_chat/${props.match.params.id}`])
 )(EventDetailedPage);
